b3257c4d11b4945014e79f6d52afd24834896eb4
lxw <ldxcwu@163.com>
2022-04-08 14:10:26 +0800

    add concurrency

--- quietHn/main.go
+++ quietHn/main.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"net/url"
 	"quietHn/hn"
+	"sort"
 	"strings"
 	"time"
 )
@@ -25,32 +26,66 @@ func main() {
 func handler(numStories int, tpl *template.Template) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
-		var c hn.Client
-		ids, err := c.GetTopStories()
+
+		td, err := getTopStories(numStories)
 		if err != nil {
-			http.Error(w, "Failed to load top stories.", http.StatusInternalServerError)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
-		}
-		var td templateData
-		for _, id := range ids {
-			item, err := c.GetItem(id)
-			if err != nil {
-				continue
-			}
-			if item.Type == "story" && item.URL != "" {
-				item.Host = url2Host(item.URL)
-				td.Stories = append(td.Stories, item)
-			}
-			if len(td.Stories) > numStories {
-				break
-			}
 		}
 		td.Time = time.Since(start).Seconds()
 		err = tpl.Execute(w, td)
 		if err != nil {
 			http.Error(w, "Failed to process the template.", http.StatusInternalServerError)
+			return
 		}
 	})
+}
+
+func getTopStories(numStories int) (templateData, error) {
+	var c hn.Client
+	var td templateData
+	ids, err := c.GetTopStories()
+	if err != nil {
+		return td, err
+	}
+	type ret struct {
+		idx  int
+		item hn.Item
+		err  error
+	}
+	retCh := make(chan ret)
+	for i := 0; i < numStories; i++ {
+		go func(i, id int) {
+			item, err := c.GetItem(id)
+			if err != nil {
+				retCh <- ret{idx: i, err: err}
+			} else {
+				retCh <- ret{idx: i, item: item}
+			}
+		}(i, ids[i])
+	}
+	var res []ret
+	for i := 0; i < numStories; i++ {
+		res = append(res, <-retCh)
+	}
+	sort.Slice(res, func(i, j int) bool {
+		return res[i].idx <= res[j].idx
+	})
+	for _, r := range res {
+		if r.err != nil {
+			continue
+		}
+		item := r.item
+		if item.Type == "story" && item.URL != "" {
+			item.Host = url2Host(item.URL)
+			td.Stories = append(td.Stories, item)
+		}
+		if len(td.Stories) > numStories {
+			break
+		}
+	}
+	close(retCh)
+	return td, nil
 }
 
 func url2Host(URL string) string {
