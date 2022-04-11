3674a2fdab6ecdbd7b4005559509ec11f181c0de
lxw <ldxcwu@163.com>
2022-04-11 15:50:56 +0800

    panic/recover middleware init

---
+++ PRMiddleWr/README.md
@@ -0,0 +1,2 @@
+# Panic/Recover Middleware
+[Check out this article to learn some basic knowledge about defer, panic and recover.](DeferPanicAndRecover.md)
---
+++ PRMiddleWr/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", hello)
+	mux.HandleFunc("/panic", panicDemo)
+	mux.HandleFunc("/panic-after", panicAfterDemo)
+
+	http.ListenAndServe(":8080", mux)
+}
+
+func panicDemo(w http.ResponseWriter, r *http.Request) {
+	panic("Oh no!")
+}
+
+func panicAfterDemo(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "<h1>Hello!</h1>")
+	panic("Oh no!")
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "<h1>Hello!</h1>")
+}
