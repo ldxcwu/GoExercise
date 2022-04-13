be5b3c084cbfc204ca9d501e8a996ff8b0846c0a
lxw <ldxcwu@163.com>
2022-04-13 11:36:51 +0800

    secret -- vault, file-backed version

--- secret/cmd/demo.go
+++ secret/cmd/demo.go
@@ -6,9 +6,21 @@ import (
 )
 
 func main() {
-	vault := secret.NewVault("my-fake-key")
+	vault := secret.NewVault("my-fake-key", ".secrets")
 	vault.Set("gophercises", "<account:ldxcwu@163.com>, <password:test>")
+	vault.Set("google", "<account:ldxcwu@163.com>, <password:lxw's google>")
+	vault.Set("gophernotes", "<account:ldxcwu@163.com>, <password:lxw's gophernotes>")
 	value, err := vault.Get("gophercises")
+	if err != nil {
+		return
+	}
+	fmt.Printf("value: %v\n", value)
+	value, err = vault.Get("google")
+	if err != nil {
+		return
+	}
+	fmt.Printf("value: %v\n", value)
+	value, err = vault.Get("gophernotes")
 	if err != nil {
 		return
 	}
--- secret/vault.go
+++ secret/vault.go
@@ -1,32 +1,103 @@
 package secret
 
 import (
+	"encoding/json"
 	"errors"
+	"io"
+	"os"
 	"secret/encrypt"
+	"strings"
+	"sync"
 )
 
 type Vault struct {
 	encodingKey string
+	filepath    string
 	kvMap       map[string]string
+	mux         sync.Mutex
 }
 
-func NewVault(encodingKey string) Vault {
-	return Vault{
+func NewVault(encodingKey, filepath string) *Vault {
+	return &Vault{
 		encodingKey: encodingKey,
-		kvMap:       make(map[string]string),
+		filepath:    filepath,
 	}
 }
 
+func (v *Vault) loadKVMap() error {
+	f, err := os.Open(v.filepath)
+	if err != nil {
+		v.kvMap = make(map[string]string)
+		return nil
+	}
+	defer f.Close()
+	// file is not nil
+	// 1. load content from file(with encrypted json)
+	// 2. decrypted file content to json
+	// 3. decode json to v.kvMap
+	var sb strings.Builder
+	_, err = io.Copy(&sb, f)
+	if err != nil {
+		return err
+	}
+	decryptedJSON, err := encrypt.Decrypt(v.encodingKey, sb.String())
+	if err != nil {
+		return err
+	}
+	dec := json.NewDecoder(strings.NewReader(decryptedJSON))
+	err = dec.Decode(&v.kvMap)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (v *Vault) saveKVMap() error {
+	var sb strings.Builder
+	err := json.NewEncoder(&sb).Encode(v.kvMap)
+	if err != nil {
+		return err
+	}
+	encryptedJSON, err := encrypt.Encrypt(v.encodingKey, sb.String())
+	if err != nil {
+		return err
+	}
+	f, err := os.OpenFile(v.filepath, os.O_RDWR|os.O_CREATE, 0755)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	_, err = f.WriteString(encryptedJSON)
+	// _, err = fmt.Fprint(f, encryptedJSON)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 func (v *Vault) Set(key, value string) error {
+	v.mux.Lock()
+	defer v.mux.Unlock()
 	encryptedVal, err := encrypt.Encrypt(v.encodingKey, value)
 	if err != nil {
 		panic(err)
 	}
+	err = v.loadKVMap()
+	if err != nil {
+		return err
+	}
 	v.kvMap[key] = encryptedVal
-	return nil
+	err = v.saveKVMap()
+	return err
 }
 
 func (v *Vault) Get(key string) (string, error) {
+	v.mux.Lock()
+	defer v.mux.Unlock()
+	err := v.loadKVMap()
+	if err != nil {
+		return "", err
+	}
 	encryptedVal, ok := v.kvMap[key]
 	if !ok {
 		return "", errors.New("no such value for that key")
@@ -36,4 +107,19 @@ func (v *Vault) Get(key string) (string, error) {
 		return "", err
 	}
 	return plaintext, nil
+}
+
+func (v *Vault) Del(key string) error {
+	v.mux.Lock()
+	defer v.mux.Unlock()
+	err := v.loadKVMap()
+	if err != nil {
+		return err
+	}
+	_, ok := v.kvMap[key]
+	if !ok {
+		return nil
+	}
+	delete(v.kvMap, key)
+	return v.saveKVMap()
 }
